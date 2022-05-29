c4235a4a351f9942cdf7fbe435f0b5f1020cac57
kenjis <kenji.uui@gmail.com>
2022-05-30 07:37:12 +0900

    refactor: use Auto Routing (Improved) for `/`

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,7 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    public function getIndex(): string
     {
         return view('welcome_message');
     }
